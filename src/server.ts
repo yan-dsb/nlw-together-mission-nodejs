622123a8391a83e1ad2f4ad5c28305a416e13011
Yan Borges <yandsb16@gmail.com>
2021-10-17 19:48:56 -0400

    status not type

--- src/server.ts
+++ src/server.ts
@@ -16,12 +16,12 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response
       .status(err.statusCode)
-      .json({ type: 'error', message: err.message });
+      .json({ status: 'error', message: err.message });
   }
   console.error(err);
   return response
     .status(500)
-    .json({ type: 'error', message: 'Internal server error' });
+    .json({ status: 'error', message: 'Internal server error' });
 });
 
 app.listen(3333, () => console.log('Server started listening on port 3333'));
